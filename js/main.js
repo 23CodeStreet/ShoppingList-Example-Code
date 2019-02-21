a0a45c24b92cd97f491204870d3309114aac66b5
Guy John <guy@rumblesan.com>
2019-02-21 08:57:59 +0000

    Add function to style the buttons as completed

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ function getTag(selector, number) {
   return el;
 }
 
+// Get all the tags for a specific selector
+// selector is a CSS style selector e.g. 'li.myclass'
+function getAllTags(selector) {
+  return document.querySelectorAll(selector);
+}
+
 // Takes the name of an item to add to the list
 // Creates the new HTML element with the name given
 // itemName is the string value with the new item's name
@@ -24,6 +30,16 @@ function addItemElementToList(itemName) {
 function getItemNameFromInput(){
   var inputElement = getTag("input", 0);
   return inputElement.value;
+}
+
+// Loop through all the items in the list and
+// set a class to style them as 'completed'
+function completeAllItems() {
+  var listItemElements = getAllTags("li");
+  for (var i = 0; i < listItemElements.length; i = i + 1) {
+    var el = listItemElements[i];
+    el.className = "list__item--completed";
+  }
 }
 
 // This is the callback that gets triggered when a
